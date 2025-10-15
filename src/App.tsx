83fa2342e14e13bf1b9ce162cce6d150fa11b820
Uri_Raz <uri1515@gmail.com>
2025-10-15 22:51:27 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="flex h-[100dvh] flex-col items-center justify-center">
+      <div className="text-preset1 text-4xl">l</div>
+      <div className="font-serif text-4xl font-bold">I</div>
+    </div>
+  )
+}
+
+export default App
